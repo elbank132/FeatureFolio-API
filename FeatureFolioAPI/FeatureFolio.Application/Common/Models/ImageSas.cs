721ab264900e4395d842a25d817f9ef36ec8c709
gal-neeman <galneeman4@gmail.com>
2026-03-23 21:06:42 +0200

    implemented storage service for creating SAS tokens for creating and uploaidng image blobs to azure storage

---
+++ FeatureFolioAPI/FeatureFolio.Application/Common/Models/ImageSas.cs
@@ -0,0 +1,7 @@
+﻿namespace FeatureFolio.Application.Common.Models;
+
+public class ImageSas
+{
+    public string UploadUrl { get; set; } = String.Empty;
+    public string BlobName { get; set; } = String.Empty;
+}
