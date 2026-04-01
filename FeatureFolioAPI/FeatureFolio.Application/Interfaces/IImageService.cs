73bba21409c141bb35756cb6b5ed9dfa8ed0531c
gal-neeman <galneeman4@gmail.com>
2026-04-01 21:34:32 +0300

    basic exception handling

--- FeatureFolioAPI/FeatureFolio.Application/Interfaces/IImageService.cs
+++ FeatureFolioAPI/FeatureFolio.Application/Interfaces/IImageService.cs
@@ -2,6 +2,6 @@
 
 public interface IImageService
 {
-    public Task<ICollection<string>> GetImageSasUrlsAsync(int amount , string userGuid);
-    public Task<bool> ValidateImagesExistAsync(string userGuid);
+    public Task<ICollection<string>> GetImageSasUrlsAsync(int amount, string userGuid);
+    public Task ProcessFinishedUploadingAsync(string userGuid);
 }
---
+++ FeatureFolioAPI/FeatureFolio.Domain/Exceptions/Exceptions/RedisExceptions.cs
@@ -0,0 +1,9 @@
+﻿namespace FeatureFolio.Domain.Exceptions.Exceptions;
+
+public class RedisInvalidDataException : Exception
+{
+    private const string _messageTemplate = "Either no data was found for key '{0}', or the data is invalid.";
+
+    public RedisInvalidDataException(string key)
+            : base(string.Format(_messageTemplate, key)) { }
+}
